05190e9b7a7a9eb876d9bd0cdfc3810d2dffa719
Indranil <indranil1102@gmail.com>
2023-11-15 22:11:33 -0400

    feat: initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello world!");
+
+        rm_tester rm_Obj = new rm_tester();
+        rm_Obj.rm();
+    }
+}
